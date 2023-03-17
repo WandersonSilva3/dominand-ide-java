b3cce491ea858e69e507790137744072d9d22e40
wandersonsilva1 <wandersonwps81@gmail.com>
2023-03-17 11:00:04 -0300

    teste

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,7 +4,7 @@ public class TiposVariaveis {
 	public static void main(String[] args) {
 
 		short numeroCurto = 1;
-		int numeroNormal = numeroCurto;
+		final int numeroNormal = numeroCurto;
 		short numeroCurto2 = (short) numeroNormal;
 		
 		System.out.println(numeroCurto2);
